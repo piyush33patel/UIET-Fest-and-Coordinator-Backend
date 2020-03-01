37aeaca85a0cb7b67db07ccbea5e072623244086
Piyush Patel <56225984+piyush33patel@users.noreply.github.com>
2020-03-01 07:28:13 +0530

    Create FetchSports.php

---
+++ FetchSports.php
@@ -0,0 +1,19 @@
+<?php
+require_once dirname(__FILE__).'/Constants.php';
+$con  =  mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+$sql = "SELECT * FROM `events` WHERE `type` LIKE 'SPORTS'";
+$result = mysqli_query($con,$sql);
+$response = array();
+while($row = mysqli_fetch_array($result))
+{
+array_push($response,array(
+                "title"=>$row["title"],
+                "type"=>$row["type"],
+                "about"=>$row["about"],
+                "date"=>$row["date"],
+                "venue"=>$row["venue"],
+                "begin"=>$row["begin"],
+                "finish"=>$row["finish"]));
+}
+echo json_encode($response);
+?>
