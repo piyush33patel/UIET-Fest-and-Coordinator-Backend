2a4630e0f004670f37704d637166599860c51380
Piyush Patel <56225984+piyush33patel@users.noreply.github.com>
2020-03-01 07:34:10 +0530

    Create Report_TotalParticipatedSports.php

---
+++ Report_TotalParticipatedSports.php
@@ -0,0 +1,13 @@
+<?php
+require_once dirname(__FILE__).'/Constants.php';
+$con  =  mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+$response = array();
+
+    $sql = "SELECT DISTINCT student.rollno,name FROM student,participates,events WHERE student.rollno=participates.rollno AND participates.title=events.title AND events.type='SPORTS'";
+    $result = mysqli_query($con, $sql);
+    while($row = mysqli_fetch_array($result))
+    {
+        array_push($response, array("rollno"=>$row["rollno"], "name"=>$row["name"]));
+    }
+echo json_encode($response);
+?>
